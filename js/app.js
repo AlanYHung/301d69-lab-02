bd99e46ed7f6485938fd47da4aa11602cb25d4b2
AlanYHung <AlanYHung@hotmail.com>
2020-12-02 16:57:48 -0800

    Feature 4 Complete

--- js/app.js
+++ js/app.js
@@ -10,9 +10,28 @@ let currentPage = imgObjArrayPage1;
   if(OptionKeyWord.length === 0 || !OptionKeyWord.includes(anoKeyword)){
     OptionKeyWord.push(anoKeyword);
     const newOption = `<option value="${anoKeyword}">${anoKeyword}</option>`;
-    $('select').append(newOption);
+    $('#title-selector').append(newOption);
   }
 };
+
+const imageTitleSort = titleSortArray => {
+  titleSortArray.sort((imgTitleObj1,imgTitleObj2) => {
+    if(imgTitleObj1.title.toLowerCase() < imgTitleObj2.title.toLowerCase()){
+      return -1;
+    }else if(imgTitleObj1.title.toLowerCase() > imgTitleObj2.title.toLowerCase()){
+      return 1;
+    }else{
+      return 0;
+    }
+  })
+};
+
+const imageHornSort = hornSortArray => {
+  hornSortArray.sort((imgHornObj1, imgHornObj2) => {
+    return imgHornObj1.horns - imgHornObj2.horns;
+  });
+};
+
 
 function JImageObject (jImgObject){
   this.image_url = jImgObject.image_url;
@@ -55,12 +74,12 @@ const refreshPageImages = (rpiInputObject = '') => {
 const refreshSelectElement = () => {
   const optionDefaultSelection = "<option value=\"default\">All Images</option>"
 
-  $('select').empty();
+  $('#title-selector').empty();
   OptionKeyWord = [];
-  $('select').append(optionDefaultSelection);
+  $('#title-selector').append(optionDefaultSelection);
 }
 
-$('select').on('change', e => {
+$('#title-selector').on('change', e => {
   refreshPageImages(e.target.value);
 });
 
@@ -76,6 +95,18 @@ $('#page-2-button').on('click', e => {
   refreshPageImages();
 })
 
+$('#sort-selector').on('change', e => {
+  if(e.target.value === "alphabetical"){
+    imageTitleSort(imgObjArrayPage1);
+    imageTitleSort(imgObjArrayPage2);
+    refreshPageImages();
+  }else{
+    imageHornSort(imgObjArrayPage1);
+    imageHornSort(imgObjArrayPage2);
+    refreshPageImages()
+  }
+});
+
 $.ajax({
   url: './data/page-1.json',
   async: true,
@@ -83,6 +114,7 @@ $.ajax({
     dataObj.forEach(imgDataObj => imgObjArrayPage1.push(new JImageObject(imgDataObj)));
   }
 }).then(() => {
+  imageTitleSort(imgObjArrayPage1);
   currentPage.forEach(imgDataObjRender => {
     imgDataObjRender.render();
   });  
@@ -94,4 +126,6 @@ $.ajax({
   success: dataObj => {
     dataObj.forEach(imgDataObj => imgObjArrayPage2.push(new JImageObject(imgDataObj)));
   }
+}).then(() => {
+  imageTitleSort(imgObjArrayPage2);
 });
