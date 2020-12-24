700efaa492c7fb1b524b54135c36bd7dadc6205d
seido <seid783@gmail.com>
2020-12-23 20:15:01 -0800

    modals

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
-
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
 const imgObjArrayPage1 = [];
 const imgObjArrayPage2 = [];
 let OptionKeyWord = [];
@@ -61,6 +63,17 @@ JImageObject.prototype.render = function(orKeyword = '') {
     
   }
   addNewOption(this.keyword);
+}
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 const refreshPageImages = (rpiInputObject = '') => {
@@ -129,3 +142,5 @@ $.ajax({
 }).then(() => {
   imageTitleSort(imgObjArrayPage2);
 });
+
+
